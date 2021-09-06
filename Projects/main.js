4afee1b9e64d7f193044b1f056f57da9e94d0e76
RaviAnand111 <yravianand111@gmail.com>
2021-09-06 00:59:31 -0400

    new changes including javascript

---
+++ Projects/main.js
@@ -0,0 +1,35 @@
+const submit = document.getElementById("submit");
+
+submit.addEventListener('click', onSubmit);
+
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const msg = document.querySelector("#msg");
+const userlist = document.getElementById('list')
+
+
+function onSubmit(e){
+
+   e.preventDefault();
+
+if(inputName.value == "" || inputEmail.value == ""){
+   
+    msg.style.visibility = "visible";
+
+   setTimeout( redMsg, 2000);
+}else {
+    const li = document.createElement('li');
+   li.appendChild(document.createTextNode(inputName.value + " : " + inputEmail.value));
+
+   userlist.appendChild(li);
+
+   inputName.value = "";
+   inputEmail.value = "";
+}
+}
+
+function redMsg(){
+   msg.style.visibility = "hidden";
+}
+
+
